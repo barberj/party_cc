197f0e74e5c0a4b599247e2cbed24e9d0ab1a0e0
barberj <barber.justin@gmail.com>
2014-01-03 22:25:34 -0500

    adjust content-length to account for pretty print

--- lib/party_cc/httparty.rb
+++ lib/party_cc/httparty.rb
@@ -7,10 +7,17 @@ module HTTParty
         .to_return(File.new('#{response_filename}'))|
     end
 
+    def decode_ruby buffer
+      buffer.string
+        .gsub('=>', ': ')
+        .gsub('nil', 'null')
+    end
+
     def pp_body body
       buffer = StringIO.new
 
       jsonified = JSON.load body
+
       PP.pp(jsonified, buffer)
 
       buffer
@@ -20,6 +27,15 @@ module HTTParty
 
       buffer
     end
+
+    def adjust_content_length_for_pretty_print rsp, net_rsp
+      headers = net_rsp.response.instance_variable_get :@raw_headers
+      headers.gsub!(/(Content-Length|content-length)(:\s+)(\d+)/, '\1\2<CONTENTLENGTH>')
+      size_without_substituion = headers.bytesize + rsp.bytesize - 15
+      size = size_without_substituion + size_without_substituion.to_s.length
+      headers.gsub!("<CONTENTLENGTH>", size.to_s)
+    end
+
 
     request_methods = ['get', 'post', 'patch', 'put', 'delete', 'head', 'options']
     request_methods.each do |method|
@@ -36,12 +52,12 @@ module HTTParty
           base_file_name = "#{method}_\#{path}_\#{stmp}.txt"
           response_file_name = "response_\#{base_file_name}"
 
-          prettied_rsp = pp_body(rsp.response.body).string
+          prettied_rsp = decode_ruby(pp_body(rsp.response.body))
+          adjusted_headers = adjust_content_length_for_pretty_print prettied_rsp, rsp
 
           File.write("stub_\#{base_file_name}", generate_stub(:#{method}, response_file_name, *args))
           File.open(response_file_name, 'w') do |f|
-            f.write rsp.response.instance_variable_get :@raw_headers
-            f.write ''
+            f.write adjusted_headers
             f.write prettied_rsp
           end
 
